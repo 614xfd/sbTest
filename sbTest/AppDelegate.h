638fa6f8e538dab146cd08719740259ac33326ca
DAA <tuxiangxue@163.com>
2019-04-22 16:39:53 +0800

    Initial commit

---
+++ sbTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sbTest
+//
+//  Created by mac on 2019/4/22.
+//  Copyright © 2019 DAA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
